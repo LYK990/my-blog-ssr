038c7322b9f22f3a73829e781d19b145dd2d7a30
lyk990 <1477435414@qq.com>
2022-04-23 20:52:37 +0800

    fix: 删除package-lock

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,13 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import VueSetupExtend from 'vite-plugin-vue-setup-extend';
+import viteCompression from 'vite-plugin-compression';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     VueSetupExtend(),
+    viteCompression(),
     AutoImport({
       resolvers: [ElementPlusResolver()]
     }),
