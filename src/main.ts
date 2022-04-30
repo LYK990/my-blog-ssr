fcb31a5ec3b142d0df9f8b3544941016ebd4eedb
lyk990 <1477435414@qq.com>
2022-04-30 21:36:27 +0800

    feat: 新增restcss

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,9 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index';
-import store from './store/index';
-// import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css'
+import { store, key } from './store';
 
 const app = createApp(App);
 app.use(router);
-app.use(store);
-// app.use(ElementPlus)
+app.use(store, key);
 app.mount('#app');
